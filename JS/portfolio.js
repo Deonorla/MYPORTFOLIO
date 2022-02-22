04772564ebd099ec3c4eb2679bd1d83ea9f65c81
deonorla <deonoluleye@gmail.com>
2022-02-22 23:33:08 +0100

    my__portfolio

--- JS/portfolio.js
+++ JS/portfolio.js
@@ -38,7 +38,90 @@ navLink.forEach( link => link.addEventListener('click', linkAction))
 
 /*================ ACCORDION SKILLS ================*/
 
-/*================ QUILIFICATION SKILLS ================*/
+const skillsContent = document.getElementById('skills__content'),
+      skillsHeader = document.querySelectorAll('.skills__header');
 
+
+function toggleSkills(){
+    let itemClass = this.parentNode.ClassName
+
+    for(i = 0; i < skillsContent.length; i++){
+        skillsContent[i].className = 'skills__content skills__close'
+    }
+    if(itemClass === 'skills__content skills__close'){
+       this.parentNode.className ='skills__content skills__open '
+    }
+}
+
+skillsHeader.forEach((el) => {
+    el.addEventListener('click', toggleSkills );
+})
+
+
+/*================ QUILIFICATION TABS ================*/
+const tabs = document.querySelectorAll('[data-target]'),
+      tabContents = document.querySelectorAll('[data-content]')
+
+ tabs.forEach(tab =>{
+     tab.addEventListener('click', ()=>{
+         const target = document.querySelector(tab.dataset.target)
+
+         tabContents.forEach(tabContent =>{
+             tabContent.classList.remove('qualification__active')
+         })
+         target.classList.add('qualification__active')
+
+         tabs.forEach(tab =>{
+             tab.classList.remove('qualification__active')
+         })
+         tab.classList.add('qualification__active')
+     })
+ })
+    
 
 /*================ SERVICES MODAL ================*/
+const modalViews  = document.querySelectorAll('.services__modal'),
+      modalBtns   = document.querySelectorAll('.services__button'),
+      modalCloses = document.querySelectorAll('.services__modal-close')
+
+
+let modal = (modalClick) => {
+    modalViews[modalClick].classList.add('active-modal')
+}      
+
+modalBtns.forEach((modalBtn, i ) =>{
+    modalBtn.addEventListener('click', ()=>{
+        modal(i)
+    })
+})  
+
+modalCloses.forEach((modalClose) => {
+    modalClose.addEventListener('click', ()=>{
+        modalViews.forEach((modalView) =>{
+            modalView.classList.remove('active-modal')
+        })
+    })
+})
+
+/*================ PORTFOLIO SWIPER ================*/
+
+
+/*================ TESTIMONIAL ================*/
+
+
+/*================ SCROLL SECTIONS ACTIVE LINK ================*/
+
+
+
+
+/*================ CHANGE BACKGROUND HEADER ================*/
+
+
+
+
+/*================ QUILIFICATION  ================*/
+
+
+
+
+/*================ QUILIFICATION TABS ================*/
