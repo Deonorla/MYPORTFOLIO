330eaf86ad4a4dac75edbe73bceb22c7229fc341
deonorla <deonoluleye@gmail.com>
2022-03-01 13:46:02 +0100

    animation

--- JS/portfolio.js
+++ JS/portfolio.js
@@ -194,3 +194,55 @@ themeButton.addEventListener('click' , ()=>{
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
+
+/*================ SCROLL ANIMATION ================*/
+
+const scrolledElements = document.querySelectorAll('.js__scroll');
+      elementInView = (el, dividend = 1) => {
+         const elementTop = el.getBoundingClientRect().top;
+          return(
+               elementTop <= (window.innerHeight || document.documentElement.clientHeight)/dividend
+          );
+      }
+
+      const elementOutofView = (el) => {
+          const elementTop = el.getBoundingClientRect().top;
+          return(
+              elementTop > (window.innerHeight || document.documentElement.clientHeight)
+          );
+      }
+
+      const displayScrollElement = (element) =>{
+          element.classList.add('scrolled');
+      }
+
+      const hideScrollElement = (element) =>{
+          element.classList.remove('scrolled');
+      }
+
+      const handleScrollAnimation = () =>{
+          scrolledElements.forEach((el) =>{
+              if(elementInView(el,1.25)){
+                  displayScrollElement(el)
+              }
+              else if(elementOutofView(el))
+              {
+                  hideScrollElement(el)
+              }
+          })
+      }
+
+      window.addEventListener('scroll', ()=>{
+          handleScrollAnimation();
+      })
+
+
+
+     const  nonScrolledElements = document.querySelectorAll('.js__nonScroll');
+
+     
+        const handleNonscroll = (element)=>{
+                element.classList.add('nonScrolled');
+        }
+
+        
